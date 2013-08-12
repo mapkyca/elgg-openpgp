2d72dfffb85362bb389d6c99bb599e7203c5a29b
Marcus Povey <marcus@marcus-povey.co.uk>
2013-08-12 13:07:34 +0100

    Changing incorrect url

--- start.php
+++ start.php
@@ -7,7 +7,7 @@
  * Users are given an opportunity, via their settings page, to upload their public key. 
  *
  * @licence GNU Public License version 2
- * @link https://github.com/mapkyca/elgg-invite-only
+ * @link https://github.com/mapkyca/elgg-openpgp
  * @link http://www.marcus-povey.co.uk
  * @author Marcus Povey <marcus@marcus-povey.co.uk>
  */
